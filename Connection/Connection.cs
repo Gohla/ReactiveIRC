068ef273b7ab852b473ce40d216e6d521d8a304f
Gabriël Konat <gabrielkonat@gmail.com>
2012-10-10 21:48:03 +0200

    Working connection.

--- Connection/Connection.cs
+++ Connection/Connection.cs
@@ -2,20 +2,22 @@
 using System.Net.Sockets;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 
 namespace ReactiveIRC.Connection
 {
-    public class Connection : IDisposable
+    public class IRCConnection : IDisposable
     {
-        private Socket _socket;
+        private TcpClient _socket = new TcpClient();
+        private Subject<String> _rawMessages = new Subject<String>();
+        private IDisposable _rawMessagesSubscription;
 
         public String Address { get; private set; }
         public ushort Port { get; private set; }
+        public IObservable<String> RawMessages { get { return _rawMessages; } }
 
-        public IObservable<String> RawMessages { get; private set; }
-
-        public Connection(String address, ushort port)
+        public IRCConnection(String address, ushort port)
         {
             if(String.IsNullOrEmpty(address))
                 throw new ArgumentNullException("address");
@@ -26,62 +28,69 @@ namespace ReactiveIRC.Connection
 
         public void Dispose()
         {
-            if(_socket == null)
-                return;
-
-            _socket.Close();
-            _socket = null;
+            Disconnected();
         }
 
         public IObservable<Unit> Connect()
         {
-            if(_socket != null)
+            if(_socket.Connected)
                 return Observable.Throw<Unit>(new InvalidOperationException("Already connected or connecting."));
 
-            _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            return _socket
+            return _socket.Client
                 .ConnectObservable(Address, Port)
-                .Do(_ => { }, ConnectedError, Connected)
+                .Do(_ => Connected(), ConnectedError)
                 ;
         }
 
         public IObservable<Unit> Disconnect()
         {
-            if(_socket == null)
+            if(!_socket.Connected)
                 return Observable.Throw<Unit>(new InvalidOperationException("No connection."));
 
-            return _socket.
-                DisconnectObservable(false)
-                .Do(_ => { }, DisconnectedError, Disconnected)
+            return _socket.Client
+                .DisconnectObservable(true)
+                .Do(_ => Disconnected(), DisconnectedError)
+                ;
+        }
+
+        private void Write(String message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message + Environment.NewLine);
+            _socket.Client
+                .SendObservable(data, 0, data.Length, SocketFlags.None)
+                .Subscribe()
                 ;
         }
 
         private void Connected()
         {
-            RawMessages = _socket.ReceiveUntilCompleted(SocketFlags.None)
+            _rawMessagesSubscription = _socket.Client.ReceiveUntilCompleted(SocketFlags.None)
                 .SelectMany(x => Encoding.UTF8.GetString(x).ToCharArray())
                 .Scan(String.Empty, (a, b) => (a.EndsWith("\r\n") ? "" : a) + b)
                 .Where(x => x.EndsWith("\r\n"))
-                .Select(buffered => String.Join("", buffered))
-                .Select(a => a.Replace("\n", ""));
+                .Select(b => String.Join("", b).Replace("\n", ""))
+                .Subscribe(_rawMessages);
+                ;
+
+            Write("USER Gohla swivvet.com  swivvet.com :Gohla");
+            Write("NICK Gohla");
         }
 
         private void ConnectedError(Exception e)
         {
             _socket.Close();
-            _socket = null;
         }
 
         private void Disconnected()
         {
             _socket.Close();
-            _socket = null;
+            _rawMessagesSubscription.Dispose();
+            _rawMessagesSubscription = null;
         }
 
         private void DisconnectedError(Exception e)
         {
-            _socket.Close();
-            _socket = null;
+            Disconnected();
         }
     }
 }
--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using ReactiveIRC.Connection;
 
-namespace ConsoleTest
+namespace ReactiveIRC.ConsoleTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
+            IRCConnection connection = new IRCConnection(args[0], Convert.ToUInt16(args[1]));
+            connection.RawMessages.Subscribe(s => Console.WriteLine(s));
+            connection.Connect().Subscribe(_ => { }, e => Console.WriteLine(e.Message), () => Console.WriteLine("Connected!"));
+
+            while(true)
+            {
+                Thread.Sleep(100);
+            }
         }
     }
 }
